c47807886e735a71b484bdbb2dda4247e1252828
YuriySko <yu.sko@yandex.ru>
2021-11-13 14:00:13 +0300

    third commit

--- src/ru/examle/test/HelloWorld.java
+++ src/ru/examle/test/HelloWorld.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 public class HelloWorld {
     public static void main(String[] args) throws IOException {
         System.out.println("Привет! Это проект без maven framework.");
-        System.out.println("После создания в конне проекта файла pom.xml");
+        System.out.println("После создания в корне проекта файла pom.xml");
+
+        FileRead1.fileRead();
+        System.out.println();
         System.out.println("и добаления его через меню Add as Maven Project");
         System.out.println("проект должен стать maven проектом");
 
-        FileRead1.fileRead();
     }
 }
